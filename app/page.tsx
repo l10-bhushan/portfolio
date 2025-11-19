a516b54ee49a9345e450e2a31fb311e8b742b3be
Bhushan <bhush98@Bhushans-MacBook-Air.local>
2025-11-19 19:37:31 +0530

    final-changes-2

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
-    <div className="flex max-w-4xl px-5 m-auto my-10 flex-col border-2 border-amber-300">
+    <div className="wrapper level-3 flex max-w-4xl px-5 m-auto my-10 flex-col max-sm:px-0">
       <FadeContent
         blur={true}
         duration={1000}
